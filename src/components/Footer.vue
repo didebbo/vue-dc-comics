9be50e06dafa533b06514fae12d535eb41a623af
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-29 18:33:35 +0200

    aggiunta bozza footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="top">
+      <div class="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eum
+        tenetur laborum, ipsam officia delectus? Laudantium est eos libero
+        soluta, perspiciatis recusandae, sit deleniti laboriosam adipisci,
+        ratione assumenda iure provident!
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="container">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate id
+        obcaecati debitis optio quisquam, repudiandae eligendi. Ut molestiae
+        laudantium officia, perspiciatis distinctio eaque, ducimus impedit enim
+        voluptatibus eveniet perferendis deleniti.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/sections/footer";
+</style>
