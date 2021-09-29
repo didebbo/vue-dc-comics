91a14e0ed14ad695c892e69beef5657e05bee040
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-29 17:12:45 +0200

    aggiunta prima bozza icons section

---
+++ src/components/Info.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eius
+      dolores, ipsam autem consectetur omnis optio, minus repellendus aliquam
+      excepturi blanditiis. Vitae dolor est itaque molestiae ipsam deserunt quas
+      illo!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/sections/info";
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <Content />
+    <Info />
   </main>
 </template>
 
 <script>
 import Content from "@/components/Content.vue";
+import Info from "@/components/Info.vue";
 
 export default {
   name: "Main",
   components: {
     Content,
+    Info,
   },
 };
 </script>
