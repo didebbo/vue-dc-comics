f6aadb0db431ee2cb16ccb89a11d242c9ea2ef8d
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-29 17:06:46 +0200

    aggiunto modulo content

---
+++ src/components/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="container">
+      <h2>Content goes here!</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/sections/content";
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <Content />
+  </main>
+</template>
+
+<script>
+import Content from "@/components/Content.vue";
+
+export default {
+  name: "Main",
+  components: {
+    Content,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/sections/main";
+</style>
