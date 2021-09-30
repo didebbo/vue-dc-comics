d1f45639cd78b00adcf3dd41460c3c40e89c54c5
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-30 16:24:12 +0200

    creato componente card

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <div class="thumb">
+      <img :src="card.thumb" :alt="card.series" />
+    </div>
+    <p>
+      {{ card.series }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: ["card"],
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  display: flex;
+  flex-direction: column;
+  width: (100% / 6);
+  padding: 1em;
+
+  .thumb {
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+  }
+
+  p {
+    margin-top: 0.5em;
+    text-transform: uppercase;
+  }
+}
+</style>
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="cards">
-    <div v-for="(card, index) in array" :key="index" class="card">
-      <div class="thumb">
-        <img :src="card.thumb" :alt="card.series" />
-      </div>
-      <p>
-        {{ card.series }}
-      </p>
-    </div>
+    <Card v-for="(card, index) in array" :key="index" :card="card" />
   </div>
 </template>
 
 <script>
+import Card from "@/components/Card.vue";
+
 export default {
   name: "Cards",
   props: ["array", "colls"],
+  components: {
+    Card,
+  },
 };
 </script>
 
@@ -22,26 +20,5 @@ export default {
 .cards {
   display: flex;
   flex-wrap: wrap;
-
-  .card {
-    display: flex;
-    flex-direction: column;
-    width: (100% / 6);
-    padding: 1em;
-
-    .thumb {
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        object-position: center;
-      }
-    }
-
-    p {
-      margin-top: 0.5em;
-      text-transform: uppercase;
-    }
-  }
 }
 </style>
