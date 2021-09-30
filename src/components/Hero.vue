1c59090c2700a42e811674a969d9a794a1d21d8c
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-30 15:04:42 +0200

    aggiunto componente hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img :src="src" :alt="alt" :style="`height:${height}`" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  props: ["src", "alt", "height"],
+};
+</script>
+
+<style scoped lang="scss">
+div {
+  width: 100%;
+  //   height: 400px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top center;
+  }
+}
+</style>
